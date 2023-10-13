1895fea54fe215dd429b5f714b8d7c05b3ffd0f5
ValeP314 <>
2023-10-13 21:25:14 +0000

    Added items app, models and fixtures

---
+++ items/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Item, Category
+
+# Register your models here.
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
