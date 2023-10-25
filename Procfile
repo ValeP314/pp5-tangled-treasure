38f28e9406abd5ffca5c449dd840a4f3108d38b7
ValeP314 <>
2023-10-25 21:18:05 +0000

    Deployment on Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tangled_treasures.wsgi:application
--- tangled_treasures/settings.py
+++ tangled_treasures/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-x2xi0t00o=ephj$)o=zpeyfc%h#2obp&(xl-8-yvt=io4e_b!*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-valep314-pp5-tangled-tre-9723tp2wub.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['tangled-treasures-v2.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,13 +121,17 @@ WSGI_APPLICATION = 'tangled_treasures.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
